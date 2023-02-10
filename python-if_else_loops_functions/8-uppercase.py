2672cd9b70f8c0c417ccd195c8deba5dd623647e
Anesu Kafesu <a.kafesu@alustudent.com>
2023-02-10 05:38:43 -0800

    8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+def uppercase(str):
+    output = ""
+
+    for char in str:
+        char_code = ord(char)
+
+        if char_code >= 97 and char_code <= 122:
+            char = chr(char_code - 32)
+
+        output += char
+
+    print(output)
