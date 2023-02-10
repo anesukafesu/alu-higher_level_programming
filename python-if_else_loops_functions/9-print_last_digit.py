4e904c9386e80b53de99dea6a4f43900a1065d23
Anesu Kafesu <a.kafesu@alustudent.com>
2023-02-10 05:52:07 -0800

    9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 
 
 def print_last_digit(number):
-    if type number == 'int':
+    if type(number) == 'int':
         last_digit = str(number)[-1]
         print(last_digit, end="")
         return last_digit
