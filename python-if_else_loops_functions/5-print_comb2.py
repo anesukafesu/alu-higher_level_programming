671f03e7a7bf906cccdc2c0b0aa7f70605a1b354
Anesu Kafesu <a.kafesu@alustudent.com>
2023-02-10 03:58:41 -0800

    5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -4,4 +4,4 @@ for i in range(100):
     output = "0" if i < 10 else ""
     output += str(i)
     output += "\n" if i == 99 else ", "
-    print(output, end="")
+    print("{}".format(output), end="")
