3a90962f98d52ec97a945832186e672d96771f5e
Anesu Kafesu <a.kafesu@alustudent.com>
2023-02-10 11:58:29 -0800

    100-fix

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,9 @@
 
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        print("{}".format(chr(i)))
+        print("{}".format(chr(i)), end="")
     else:
-        print(f"{chr(i - 32)}")
+        print(f"{chr(i - 32)}", end="")
+
+    if i == 97:
+        print("")
