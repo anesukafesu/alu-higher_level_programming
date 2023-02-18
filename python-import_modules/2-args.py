69c1f057773e57595a65cfa536bc3bfa2c804534
Anesu Kafesu <a.kafesu@alustudent.com>
2023-02-18 07:27:36 -0800

    2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    number_of_arguments = len(sys.argv) - 1
+    print("{}".format(number_of_arguments), end=" ")
+
+    
+    if number_of_arguments == 0:
+        print("arguments.")
+    elif number_of_arguments == 1:
+        print("argument:")
+    else:
+        print("arguments:")
+    
+
+    for i in range(1, number_of_arguments + 1):
+        print("{}: {}".format(i, sys.argv[i]))
+
+
+if __name__ == "__main__":
+    main()
