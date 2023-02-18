aabb0652522d36c169109d10cc17de8a17cafe15
Anesu Kafesu <a.kafesu@alustudent.com>
2023-02-18 08:04:16 -0800

    4

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from inspect import getmembers, isfunction
+import hidden_4
+
+
+def main():
+    members = getmembers(hidden_4, isfunction)
+    
+    for member in members:
+        name = member[0]
+
+        # Not printing the name if it starts with an underscore
+        if name[0] == '_':
+            continue
+
+        print(name)
+
+
+if __name__ == '__main__':
+    main()
