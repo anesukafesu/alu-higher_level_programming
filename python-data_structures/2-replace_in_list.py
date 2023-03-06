d94306934cac9f97e8287d0e043288ee5583c58b
anesu <anesukafesu@gmail.com>
2023-03-06 19:38:20 +0200

    2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -5,6 +5,5 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        new_array = my_list.copy()
-        new_array[idx] = element
-        return new_array
+        my_list[idx] = element
+        return my_list
