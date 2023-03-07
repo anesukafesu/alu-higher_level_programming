165e5f23bb72f803ecc5de8822cda8425e5d8a74
anesu <anesukafesu@gmail.com>
2023-03-08 01:32:35 +0200

    3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,6 +6,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except:
-        print("Exception")
+        pass
     finally:
         print("Inside result: {}".format(result))
+        return result
