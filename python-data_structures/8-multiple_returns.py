dfa64d14861d1c0bcdb593a381de21d0c6427d61
anesu <anesukafesu@gmail.com>
2023-03-07 14:05:44 +0200

    8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    sentence_length = len(sentence)
+
+    if sentence_length == 0:
+        return (0, None)
+    else:
+        return (sentence_length, sentence[0])
