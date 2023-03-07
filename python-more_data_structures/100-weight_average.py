1b898f39beec10ce768923a13518a7a70a0a9099
anesu <anesukafesu@gmail.com>
2023-03-08 00:49:05 +0200

    100

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -8,5 +8,5 @@ def weight_average(my_list=[]):
     for record in my_list:
         total += record[0] * record[1]
         item_count += record[1]
-    
+
     return 0 if item_count == 0 else total / item_count
