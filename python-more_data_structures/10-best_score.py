ad7a96219ecaa1f34ed99e620a208688a6169390
anesu <anesukafesu@gmail.com>
2023-03-07 18:03:58 +0200

    10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    max_score = -9223372036854775806
+
+    for key in a_dictionary:
+        if a_dictionary[key] > max_score:
+            return person
+
+    return None
