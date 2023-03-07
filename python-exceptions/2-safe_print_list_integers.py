653bcef2bceb7e71c974ceb0a8bd6da0768420e4
anesu <anesukafesu@gmail.com>
2023-03-08 00:13:24 +0200

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
@@ -8,6 +9,8 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except ValueError:
             pass
-    
+        except TypeError:
+            pass
+
     print()
     return count
