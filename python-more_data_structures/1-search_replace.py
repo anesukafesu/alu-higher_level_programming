464bddf650a45f8d28ec6b2ffafa032ccbd819f6
anesu <anesukafesu@gmail.com>
2023-03-07 14:59:18 +0200

    1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,10 +2,12 @@
 
 
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
+    new_list = []
 
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] == replace
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
 
     return new_list
