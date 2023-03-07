f75dc560b04f7a9e5ffa8f98388cd4e2cc20ea62
anesu <anesukafesu@gmail.com>
2023-03-07 22:52:05 +0200

    12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    # Getting rid of empty strings and invalid data types
+    if type(roman_string) == 'str' or str == '':
+        return 0
+    
+    # From this point, we can assume the string has at least one char
+    mappings = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    last_index = len(roman_string) - 1
+
+    for i in range(last_index + 1):
+        curr_val = mappings[roman_string[i]]
+        
+        if i < last_index:
+            next_val = mappings[roman_string[i + 1]]
+
+            if curr_val < next_val:
+                total -= curr_val
+            else:
+                total += curr_val
+        else:
+            total += curr_val
+
+    print(total)
+
+roman_to_int('XXVIII')
