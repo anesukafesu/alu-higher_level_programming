7cf368cc06b8aa0f4dd96bac04fa1e2199c05dd2
anesu <anesukafesu@gmail.com>
2023-03-07 14:52:00 +0200

    0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def square_array(array):
-    return map(lambda x: x ** 2, array)
+    return list(map(lambda x: x ** 2, array))
 
 
 def square_matrix_simple(matrix=[]):
-    return map(square_array, matrix)
+    return list(map(square_array, matrix))
