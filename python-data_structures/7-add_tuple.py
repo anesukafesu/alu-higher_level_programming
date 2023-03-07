35a656acd2f0fd7c32a65fc5f188bef388fdff64
anesu <anesukafesu@gmail.com>
2023-03-07 13:59:03 +0200

    7-fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -16,6 +16,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = (tuple_b[0], 0)
 
     first_element = tuple_a[0] + tuple_b[0]
-    second_element = tuple_b[1] + tuple_b[1]
+    second_element = tuple_a[1] + tuple_b[1]
 
     return (first_element, second_element)
