f1cdb66d644d8277050a280834c9e9e7097efcb3
anesu <anesukafesu@gmail.com>
2023-03-08 00:08:21 +0200

    12

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-
 def safe_print_list_integers(my_list=[], x=0):
     i = -1
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except:
+        except ValueError:
             pass
     
     print()
