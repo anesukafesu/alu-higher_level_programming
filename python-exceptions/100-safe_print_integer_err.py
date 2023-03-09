977c67d678911955611ead5f624ec1d7f988187f
anesu <anesukafesu@gmail.com>
2023-03-09 13:10:42 +0200

    100-safe_print_integer_err.py

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except Exception as err:
         print('Exception: {}'.format(err))
         return False
 
