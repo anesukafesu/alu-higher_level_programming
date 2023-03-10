a4cac12775180ed70103afd6d3a14b7c5804d24b
anesu <anesukafesu@gmail.com>
2023-03-10 21:31:08 +0200

    102

---
+++ python-exceptions/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+def magic_calculation(a, b):
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if a > i:
+                raise Exception('Too far')
+            else:
+                result = result + (a ** b) / i
+        except:
+            result = a + b
+            break
+
+        
+    return result
