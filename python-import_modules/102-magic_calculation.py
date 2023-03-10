d6965c4806fc65995613809a7b169b41eb57cff7
anesu <anesukafesu@gmail.com>
2023-03-10 22:05:45 +0200

    102

---
+++ python-import_modules/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    _temp = __import__('magic_calculation_102', ('add', 'sub'), 0)
+    add = _temp.add
+    sub = _temp.sub
+
+    if a > b:
+        c = add(a, b)
+
+        for i in range(4, 6):
+            c = add(c, i)
+
+        return c
+    else:
+        return sub(a, b)
