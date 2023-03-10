cece10f9de34758c14a77fd120300e089af93103
anesu <anesukafesu@gmail.com>
2023-03-10 21:35:37 +0200

    102

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -5,7 +5,7 @@ def magic_calculation(a, b):
 
     for i in range(1, 3):
         try:
-            if a > i:
+            if i > a:
                 raise Exception('Too far')
             else:
                 result = result + (a ** b) / i
