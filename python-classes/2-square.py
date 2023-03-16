baabd79977fba494c44d5e3509d9949c47a52080
anesu <anesukafesu@gmail.com>
2023-03-16 23:20:28 +0200

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin
+"""
+This module also demonstrates creating Python classes
+Using the constructor method and validating arguments
+"""
+
+
+class Square:
+    """
+    This square validates data
+    """
+
+    def __init__(self, size=0):
+        """
+        The constructor takes in a size int as an arg
+        If size < 0 a ValueError exception will be raised
+        If size is not an int a TypeError exception will be raised
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        
+        self.__size = size
