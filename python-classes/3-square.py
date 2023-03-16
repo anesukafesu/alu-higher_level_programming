f3bd6e2abaf6575e63243d61adb9b1c710dd6b37
anesu <anesukafesu@gmail.com>
2023-03-16 23:34:49 +0200

    3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,24 +7,22 @@ calculate its own area using our special
 proprietary algorithm that has been patented
 """
 
+
 class Square:
     """
     This patented square class
     """
-    def __init__(self, size=0):
-        
+    def __init__(self, size=0):        
         """
-        Takes in size and ensures it is the right data type and is in the right range
+        Takes in size and ensures it is the right
+        data type and is in the right range
         """
         # Check if size is not an int
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
-        
         # Check if size is negative
         if size < 0:
             raise ValueError('size must be >= 0')
-
-
         self.__size = size
 
     def area(self):
