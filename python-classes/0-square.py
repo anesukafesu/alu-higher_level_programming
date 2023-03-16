c0cdd01115ef96ae3482b3e3c73f5ad92a2f734d
anesu <anesukafesu@gmail.com>
2023-03-16 18:02:04 +0200

    0

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Empty classes
+This example demonstrates the declaration of classes in Python.
+"""
+
+
+class Square():
+    """
+    This is an empty square class
+    """
+    pass
