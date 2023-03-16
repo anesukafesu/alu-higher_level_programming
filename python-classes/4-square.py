e4d168598769bfd3091970ad75d7dfe9519362d9
anesu <anesukafesu@gmail.com>
2023-03-17 00:20:54 +0200

    4

---
+++ python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+Another world-changing square module
+"""
+
+
+class Square:
+    """
+    Here's the good part
+    """
+    def __init__(self, size=0):
+        """
+        Size property is optional because size
+        does not matter
+        """
+        self.__size = size
+
+
+    def size(self):
+        """
+        Square v2 now can retrieve properties
+        """
+        return self.__size
+
+
+    def size(self, value):
+        """
+        And you can set them too using 
+        a fancy concept called polymorphism
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
