2f81a6008437a72bebfc2d0cb04e158203b58ad0
anesu <anesukafesu@gmail.com>
2023-03-17 23:18:45 +0200

    103

---
+++ python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+Implements a circle
+I am doing this exercise at 2 3.14hrs
+"""
+
+import math
+
+class MagicClass:
+    """
+    This is a magic class
+    """
+    def __init__(self, radius):
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        
+        self.radius = radius
+
+    def area():
+        return math.pi * radius ** 2
+
+    def circumference():
+        return math.pi * radius * 2
