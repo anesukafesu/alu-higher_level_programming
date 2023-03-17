0bee39509427ec207de4a0f260cf03c395a10a71
anesu <anesukafesu@gmail.com>
2023-03-17 17:34:28 +0200

    8

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -43,7 +43,7 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         def raise_error(rect):
-            throw TypeError(f'{rect} must be an instance of Rectangle')
+            raise TypeError(f'{rect} must be an instance of Rectangle')
 
         if not isinstance(rect_1, Rectangle):
             raise_error('rect_1')
