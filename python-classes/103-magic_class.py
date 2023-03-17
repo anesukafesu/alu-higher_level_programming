dd72bca063d88a32bf900fc22546fabe1b0f4734
anesu <anesukafesu@gmail.com>
2023-03-17 23:23:46 +0200

    103

--- python-classes/103-magic_class.py
+++ python-classes/103-magic_class.py
@@ -6,6 +6,7 @@ I am doing this exercise at 2 3.14hrs
 
 import math
 
+
 class MagicClass:
     """
     This is a magic class
@@ -13,7 +14,7 @@ class MagicClass:
     def __init__(self, radius=0):
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        
+
         self.radius = radius
 
     def area(self):
