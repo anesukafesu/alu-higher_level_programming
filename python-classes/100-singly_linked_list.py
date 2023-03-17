5b26d07604ea41c140e6d8672c13f6050e2dbb57
anesu <anesukafesu@gmail.com>
2023-03-17 12:04:23 +0200

    100

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Node and SinglyLinkedList class of a linked list
+"""
+
+class Node:
+    """
+    class Node
+    stores value as data
+    stores a reference to the next node in next_node
+    if it is the terminal node then next_node will be none
+    """
+    def __init__(self, data, next_node=None):
+        self.data(data)
+        self.next_node(next_node)
+    
+    @property
+    def data(self):
+        return self.__data
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError('data must be an integer')
+
+        self.__data = value
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node):
+            raise TypeError('next_node must be a Node object')
+
+    def __repr__(self):
+        return self.data()
+
+
+class SinglyLinkedList:
+    """
+    implements a linked list
+    allows simple operations like inserting items
+    and printing out values into the terminal
+    """
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        if self.__head = None:
+            self.__head = Node(value)
+        else:
+            # We maintain a pointer to the previous node for adding
+            prev = self.__head
+
+            curr = self.__head.next_node
+
+            while True:
+                if curr == None:
+                    prev.next_node = Node(value)
+                    break
+                else:
+                    prev = curr
+                    curr = curr.next_node
+
+    def __repr__(self):
+        output = ""
+        node = self.__head
+        
+        while True:
+            if node == None:
+                break
+            else:
+                output += f'{node}\n'
+                node = node.next
+
