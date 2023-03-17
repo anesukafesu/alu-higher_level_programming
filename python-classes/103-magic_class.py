8af66d90bdf12187609f7e304bf48ba7e862287a
anesu <anesukafesu@gmail.com>
2023-03-17 23:20:15 +0200

    103

--- python-classes/103-magic_class.py
+++ python-classes/103-magic_class.py
@@ -10,7 +10,7 @@ class MagicClass:
     """
     This is a magic class
     """
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         
