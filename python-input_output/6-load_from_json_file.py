5a7cad082d0e2404d3c46dc2c16ae9dd35aef8e2
anesu <anesukafesu@gmail.com>
2023-03-23 21:46:55 +0200

    6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -9,13 +9,13 @@ import json
 
 def load_from_json_file(filename):
     """
-    Returns the object represented by the JSON in the 
+    Returns the object represented by the JSON in the
     specified file.
 
     Args:
         - filename(str) - The name of the file that
         contains the JSON
-    
+ 
     Returns:
         - Object - The object represented by the JSON
         in the specified file
