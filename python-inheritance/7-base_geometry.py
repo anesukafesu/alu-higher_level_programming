4c2e6fcfa6e0f433eb0279c55ad2fb704d4aed4e
anesu <anesukafesu@gmail.com>
2023-03-23 08:58:52 +0200

    7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -18,5 +18,5 @@ class BaseGeometry:
         if type(name) != str:
             raise TypeError(f'{name} must be an integer')
 
-        if value < 0:
+        if value <= 0:
             raise ValueError(f'{name} must be greater than 0')
