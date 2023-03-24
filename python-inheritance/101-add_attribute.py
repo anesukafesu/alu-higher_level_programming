e53cdf0a071f0a2cfd57171c9af0086210df57f4
anesu <anesukafesu@gmail.com>
2023-03-24 23:49:20 +0200

    101

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Gently adds attributes
+"""
+
+def add_attribute(obj, key, value):
+    """
+    Consent culture
+    """
+    if hasattr(obj, name):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
