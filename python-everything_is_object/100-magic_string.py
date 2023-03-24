c92c6e2e8567bb5fcd9b3829a14b4b56ee1b3cb8
anesu <anesukafesu@gmail.com>
2023-03-24 16:22:36 +0200

    100

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.c = 1 if magic_string.c is None else magic_string.c + 1
-    return ("BestSchool, " * (magic_string.c - 1)) + "BestSchool"
+    return ("BestSchool, " * ((magic_string.c := magic_string.c + 1) - 1)) + "BestSchool"
+magic_string.c = 0
