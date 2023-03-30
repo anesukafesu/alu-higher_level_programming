8991adc0e27b6c122e368cab78b76a1cb8de3dfe
anesu <anesukafesu@gmail.com>
2023-03-31 01:31:46 +0200

    7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,3 +1,6 @@
+-- creating cities table with state_id as a foreign key column
+-- references id of state table
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
