0659a22e8a9b8fdc000baf4541adf578a92fdbb4
anesu <anesukafesu@gmail.com>
2023-03-31 01:33:01 +0200

    7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	name VARCHAR(255),
 	state_id INT,
+	name VARCHAR(255),
 	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
