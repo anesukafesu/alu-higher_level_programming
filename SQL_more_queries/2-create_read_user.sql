ac1e3b237febeb8ccc3092d565c1721f08e4fea2
anesu <anesukafesu@gmail.com>
2023-03-31 00:19:39 +0200

    2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@
 -- grant select permission to user_0d_2 over hbtn_0d_2
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
