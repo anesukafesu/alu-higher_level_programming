5148f71935cf52b6381dcb89d3ddef5e954b2d7b
anesu <anesukafesu@gmail.com>
2023-03-31 00:40:58 +0200

    6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,4 +2,4 @@
 -- create table states inside htbn_0d_usa
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_od_usa.states(id INT UNIQUE AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT UNIQUE AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
