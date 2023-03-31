bda2d0b4ff57c81a7cdd308ce6d5f7f7536a5948
anesu <anesukafesu@gmail.com>
2023-03-31 03:25:25 +0200

    13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- counting the shows in each genre
+
+SELECT tv_genres.name as genre, COUNT(1) as number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC
