777fd45d6535c8d0d6d93dcb884c54ad48125af4
anesu <anesukafesu@gmail.com>
2023-03-31 03:05:44 +0200

    11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- displaying all shows and their genre ids
+-- those without a genre id are displayed with a null
+-- a left join will be appropriate given tv_shows is the left table
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres.genre_id
+ON tv_shows.id = tv_show_genres.show_id
