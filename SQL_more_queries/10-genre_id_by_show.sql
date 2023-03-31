20c2bdc4445e8af7a68866b85ef346840e4412bf
anesu <anesukafesu@gmail.com>
2023-03-31 02:56:34 +0200

    10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- displaying tv shows and their genres
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
