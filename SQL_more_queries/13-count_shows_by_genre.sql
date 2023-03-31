1c7c479fac85a8b9adfdd76df9db57e60a348cd0
anesu <anesukafesu@gmail.com>
2023-03-31 03:26:01 +0200

    13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- counting the shows in each genre
 
-SELECT tv_genres.name as genre, COUNT(1) as number_of_shows
+SELECT tv_genres.name AS genre, COUNT(1) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
