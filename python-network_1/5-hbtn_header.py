c83e0c672b3964793bf9cfc571f47e0a489efb30
anesu <anesukafesu@gmail.com>
2023-04-07 20:56:00 +0200

    5

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Make a request and get a header from the response"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    request_header_value = response.headers.get('X-Request_Id')
+    print(request_header_value)
