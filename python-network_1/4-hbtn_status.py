b1bd221fc22856c965172c694d0013cc40f09a07
anesu <anesukafesu@gmail.com>
2023-04-07 20:36:21 +0200

    4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
     response = requests.get("https://alu-intranet.hbtn.io/status")
-    data = response.text()
+    data = response.text
 
     print('Body response:')
     print(f'\t- type: {type(data)}')
