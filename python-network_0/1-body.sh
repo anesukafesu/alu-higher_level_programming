c36b0a0f72db734d8463b247cf478ecc3d0cf4f0
anesu <anesukafesu@gmail.com>
2023-04-07 13:41:34 +0200

    1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Printing request body if it is 200
-response=$(curl -s -o /dev/null -w "%{http_code}" "http://94fe1136c97d.57e96196.alu-cod.online:5000/") && if [ $response -eq 200 ]; then echo $(curl -s "http://94fe1136c97d.57e96196.alu-cod.online:5000/"); fi
+response=$(curl -s -o /dev/null -w "%{http_code}" $1) && if [ $response -eq 200 ]; then echo $(curl -s $1); fi
