65eda92461f23fa6f5ab7b25617bfd628bbf3843
anesu <anesukafesu@gmail.com>
2023-04-07 15:14:28 +0200

    5

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send POST request with body
-curl -X POST $1 -d "email=test@gmail.com&subject=I+will+always+be+here+for+PLD"
+curl -s -X POST $1 -d "email=test@gmail.com&subject=I+will+always+be+here+for+PLD"
