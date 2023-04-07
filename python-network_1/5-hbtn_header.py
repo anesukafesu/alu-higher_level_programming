076d33da32355ab5f73b162a54b2c8ec7c1a84c1
anesu <anesukafesu@gmail.com>
2023-04-07 20:58:05 +0200

    5

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -7,5 +7,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    request_header_value = response.headers.get('X-Request_Id')
+    request_header_value = response.headers.get('X-Request-Id')
     print(request_header_value)
