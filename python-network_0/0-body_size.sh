a44a1fba7073b8b179e4b8ac8fd2d6668c631fdc
anesu <anesukafesu@gmail.com>
2023-04-07 11:12:08 +0200

    0

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# 1. Makes a get request to whatever url is specified as the first arg
-# 2. Gets the line with content length from the response using grep
-# 3. Extracts the value field using cut
-# 4. Removes any whitespaces before and after the actual value
-
+# Makes a get request to whatever url is specified as the first arg, gets the line with content length from the response using grep, extracts the value field using cut, removes any whitespaces before and after the actual value
 curl -sI $1 | grep 'content-length' | cut -d ':' -f 2 | tr -d " "
