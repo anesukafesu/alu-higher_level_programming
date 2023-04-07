26a91c1605e16cb0af31bd568f88fcf5223fa5d1
anesu <anesukafesu@gmail.com>
2023-04-07 20:27:15 +0200

    3

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         with request.urlopen(url) as response:
             print(response.read().decode("utf-8"))
     except error.HTTPError as e:
-        print(e.code)
+        print('Error code:', e.code)
