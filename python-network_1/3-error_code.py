744641662aa8fdd0d552c6acd2c87426ad61169d
anesu <anesukafesu@gmail.com>
2023-04-07 20:22:13 +0200

    3

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -6,10 +6,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
+
     try:
         with request.urlopen(url) as response:
             print(response.read().decode("utf-8"))
     except urllib.error.URLError as e:
         print(e.code)
-
