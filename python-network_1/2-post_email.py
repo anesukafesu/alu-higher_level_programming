a71b929d3bc7ae8281dbc1abce6c25c463cf278d
anesu <anesukafesu@gmail.com>
2023-04-07 20:08:58 +0200

    2

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Sending post request"""
+from urllib import request, parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = parse.urlencode({ 'email': email }).encode()
+    req = request.Request(url, data=data)
+
+    with request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
