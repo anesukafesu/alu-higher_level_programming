26bcd3df4c1a7431b5617be0e6d40103e0564c4b
anesu <anesukafesu@gmail.com>
2023-04-07 18:20:47 +0200

    0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,9 +5,10 @@ Making an HTTP request using urllib
 import urllib.request
 
 
-with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
-    content = response.read()
-    print('Body response:')
-    print(f'\t- type: {type(content)}')
-    print(f'\t- content: {content}')
-    print(f'\t- utf8 content: {content.decode("utf-8")}')
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+        content = response.read()
+        print('Body response:')
+        print(f'\t- type: {type(content)}')
+        print(f'\t- content: {content}')
+        print(f'\t- utf8 content: {content.decode("utf-8")}')
