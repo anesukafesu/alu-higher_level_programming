f4687b88e01dc7b1f2aa29f9ce752d49395aa23f
anesu <anesukafesu@gmail.com>
2023-04-07 18:39:06 +0200

    1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Getting header values from response"""
-import urllib
+import urllib.request
 import sys
 
 
 if __name__ == "__main__":
-    with urllib.request.open(sys.argv[1]) as request:
-        print(request.get_header('X-Request_Id'))
+    with urllib.request.open(sys.argv[1]) as response:
+        print(response.get_header('X-Request_Id'))
