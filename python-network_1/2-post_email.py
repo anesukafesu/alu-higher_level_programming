ebd9a2ccb0e4a90ccfd2cc57392dab415c935c33
anesu <anesukafesu@gmail.com>
2023-04-07 20:12:50 +0200

    2

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = parse.urlencode({ 'email': email }).encode()
+    data = parse.urlencode({'email': email}).encode()
     req = request.Request(url, data=data)
 
     with request.urlopen(req) as response:
