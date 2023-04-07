8fc8826c3471454dddb65550f8d3b5780e36b686
anesu <anesukafesu@gmail.com>
2023-04-07 14:09:29 +0200

    3

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Getting all the options a server accepts
-curl -si -X OPTIONS http://94fe1136c97d.57e96196.alu-cod.online:5000 | grep 'Allow' | cut -d ':' -f 2 | xargs
+curl -si -X OPTIONS $1 | grep 'Allow' | cut -d ':' -f 2 | xargs
