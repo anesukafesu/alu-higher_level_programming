8cef2f47d2594ca2ec5a4f3c892e3ef8434f504e
anesu <anesukafesu@gmail.com>
2023-04-22 17:13:00 +0200

    4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,6 +5,10 @@ Implements the print_square function
 
 
 def print_square(size):
+    """
+    prints #s in the shape of a square
+    length and width are equal to the size parameter
+    """
     if type(size) is not int:
         raise TypeError('size must be an integer')
 
