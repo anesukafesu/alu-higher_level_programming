60421422688bdc4fb16e04a36ef4069485acad2d
anesu <anesukafesu@gmail.com>
2023-04-22 16:09:16 +0200

    2

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Says your name out loud
+"""
+def say_my_name(first_name, last_name=""):
+    try:
+        if type(first_name) is not str:
+            raise Exception('first_name')
+
+        if type(last_name) is not str:
+            raise Exception('last_name')
+    except Exception as e:
+        raise TypeError(f'{e} must be a string')
+
+    print(f'My name is {first_name} {last_name}')
