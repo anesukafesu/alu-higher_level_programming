e4d4f9a1b68629da1ae89aed1dd9cf22d4d06ba8
anesu <anesukafesu@gmail.com>
2023-04-23 17:01:37 +0200

    5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Implements the text_indentation method
+"""
+
+
+def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
+    if text == '':
+        return
+
+    special_chars = ':,?'
+
+    for special_char in special_chars:
+        text = text.replace(special_char, f'{special_char}\n\n\n')
+
+    lines = text.split('\n')
+
+    for line in lines:
+        print(line.lstrip().rstrip())
