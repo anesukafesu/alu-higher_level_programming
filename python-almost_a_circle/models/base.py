dc0dbf781fd1b809d5a3a2e3cdcd893501404693
anesu <anesukafesu@gmail.com>
2023-04-25 16:07:51 +0200

    5

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,13 +3,13 @@
 Implements the base model
 """
 
+
 class Base:
     """
     Base class
     """
     __nb_objects = 0
 
-    
     def __init__(self, id=None):
         if id is not None:
             self.id = id
