cb9ecc48fb78b1c1060f70f58cd885b54e705237
anesu <anesukafesu@gmail.com>
2023-04-28 23:11:38 +0200

    10

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,7 +2,7 @@
 """
 Implements the base model
 """
-from json import dumps, loads
+import json
 
 
 class Base:
@@ -20,13 +20,11 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """
-        Converts a passed dictionary to json string
-        """
-        if list_dictionaries is None or list_dictionaries == []:
+        """Doc"""
+        if list_dictionaries is None or \
+                len(list_dictionaries) == 0:
             return "[]"
-        else:
-            return dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
@@ -49,7 +47,7 @@ class Base:
         if json_string is None:
             return []
         else:
-            return loads(json_string)
+            return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
