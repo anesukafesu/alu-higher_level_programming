fbc07bac71cf021ce0bb611a15f331cd733c8a94
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2023-06-09 02:46:27 +0300

    Update 3-value_argument.js

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
 
-let firstArg = '';
-
-try {
-  firstArg = process.argv[2];
-} catch (e) {
-  // Do nothing 🙂
-}
-
-console.log(firstArg || 
-  
+let firstArg = process.argv[2];
+console.log(firstArg || 'No argument');
