6b666146555d5a48569da02267f11633fc2d7082
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2023-06-15 21:38:06 +0000

    fixes

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-function add(a, b) {
+function add (a, b) {
   console.log(a + b);
 }
 
-let [a, b] = process.argv.slice(2, 4).map(n => Number(n));
+const [a, b] = process.argv.slice(2, 4).map(n => Number(n));
 add(a, b);
