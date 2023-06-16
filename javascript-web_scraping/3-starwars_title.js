4dff8f551e68ceea35c6f309b6ffd0c6562415fd
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2023-06-16 18:21:37 +0000

    fixes

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const request= require('request');
+const request = require('request');
 
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/:${id}`;
 
 request(url, (err, res, body) => {
-  console.log(JSON.parse(body).title);
+  console.log(body);
 });
