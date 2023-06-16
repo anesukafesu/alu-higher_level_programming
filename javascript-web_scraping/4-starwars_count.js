328de4450d64d22abf31099878c1df51236ddc8e
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2023-06-16 18:42:07 +0000

    4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (err, res, body) {
+  if (err) console.log(err);
+  else {
+    const characterId = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    for (const film of films) {
+      if (film.characters.includes(characterId)) count++;
+    }
+  }
+});
