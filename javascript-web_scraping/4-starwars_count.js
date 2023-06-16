02d2e85c2f7273e977a6f95ed3804aaa13abd42b
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2023-06-16 18:55:44 +0000

    4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -12,6 +12,7 @@ request(url, function (err, res, body) {
     let count = 0;
 
     for (const film of films) {
+      console.log(film.characters)
       if (film.characters.includes(characterId)) count++;
     }
 
