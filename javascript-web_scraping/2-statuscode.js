67d3354959ae69adbcdc14903c0095e92c1732b2
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2023-06-16 18:14:22 +0000

    fixes

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,7 @@ const { request } = require('http');
 
 const url = process.argv[2];
 
-request(url, function (res) {
-  console.log(res.statusCode);
+request(url, function (err, res, body) {
+  if (err) console.log(err);
+  else console.log(res.statusCode);
 });
