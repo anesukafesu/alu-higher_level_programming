e1ffbf7226de7ae142c2d70577a88c62a640559e
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2023-06-16 17:58:23 +0000

    made code more concise

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,5 +4,5 @@ const { readFile } = require('fs');
 const filePath = process.argv[2];
 
 readFile(filePath, 'utf-8', function (error, text) {
-  console.log(error ? error : text);
+  console.log(error || text);
 });
